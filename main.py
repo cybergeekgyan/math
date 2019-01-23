2208ccbbdc04385cf519708e644479dc35f90d14
ravigitte <42976620+ravigitte@users.noreply.github.com>
2019-01-23 13:40:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os, sys
+
+# Force sys.path to have our own directory first, in case we want to import
+# from it.
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Must set this env var *before* importing any part of Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
